466967d1987364d70be413af0db230462872c0d0
Almedin <109563696+sync0605@users.noreply.github.com>
2024-06-08 15:23:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,64 @@
+
 const menuIcon = document.querySelector('#menu-icon');
 const navbar = document.querySelector('.navbar');
 
 menuIcon.onClick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active')
+}
+
+async function sendContact(ev) {
+    ev.preventDefault();
+    const senderEmail = document
+        .getElementById('emailInput').value;
+    const senderMessage = document
+        .getElementById('messageInput').value;
+    const senderNumber = document
+        .getElementById('numberinput').value;
+    const subject = document
+        .getElementById('subjectinput').value;
+    const senderName = document
+        .getElementById('nameinput').value;
+
+    const webhookBody = {
+        embeds: [{
+            title: 'Contact Form Submitted',
+            fields: [
+                {
+                    name: 'Sender Name',
+                    value: senderName
+                },
+
+                {
+                    name: 'Sender Email',
+                    value: senderEmail
+                },
+                {
+                    name: 'Sender Number',
+                    value: senderNumber
+                },
+                {
+                    name: 'Sender Subject',
+                    value: subject
+                },
+                {
+                    name: 'Sender Message',
+                    value: senderMessage
+                }
+            ]
+        }],
+    };
+    const webhookUrl = 'https://discordapp.com/api/webhooks/1249108743644512256/QD4ewuStl2WUJFof3Ov5KEvGwUJWubXP6xw7qnXTVckHeGO4JlqqQLPfU7cp5OdXAmgj';
+    const response = await fetch(webhookUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(webhookBody),
+    });
+    if (response.ok) {
+        alert('I have received your message!');
+    } else {
+        alert('There was an error! Try again later!');
+    }
 }
